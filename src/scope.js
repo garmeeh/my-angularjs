464952aef7ba58d1cc517ad409f5d470f3bc2673
Gary Meehan <garymeehan@outlook.com>
2016-02-15 21:37:28 +0000

    Short circuit the watch when last was clean

--- src/scope.js
+++ src/scope.js
@@ -7,6 +7,7 @@ function initWatchVal(){ }
 
 function Scope() {
   this.$$watchers = [];
+  this.$$lastDirtyWatch = null;
 }
 
 Scope.prototype.$watch = function(watchFn, listenerFn) {
@@ -16,11 +17,13 @@ Scope.prototype.$watch = function(watchFn, listenerFn) {
     last: initWatchVal
   };
   this.$$watchers.push(watcher);
+  this.$$lastDirtyWatch = null;
 };
 
 Scope.prototype.$digest = function(){
   var ttl = 10;
   var dirty;
+  this.$$lastDirtyWatch = null;
   do {
     dirty = this.$$digestOnce();
     if(dirty && !(ttl--)){
@@ -37,11 +40,14 @@ Scope.prototype.$$digestOnce = function() {
       newValue = watcher.watchFn(self);
       oldValue = watcher.last;
       if(newValue !== oldValue){
+        self.$$lastDirtyWatch = watcher;
         watcher.last = newValue;
         watcher.listenerFn(newValue,
           (oldValue === initWatchVal ? newValue : oldValue),
           self);
         dirty = true;
+      } else if(self.$$lastDirtyWatch === watcher){
+        return false;
       }
   });
   return dirty;
